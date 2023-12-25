c7a1c2dab43c07f036abc99d023f310cb6612640
Aryan <102355469+Aryan0288@users.noreply.github.com>
2023-12-25 18:51:11 +0530

    Update Chat.jsx
    https://mern-chat-app-api-g3qd.onrender.com

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -25,7 +25,8 @@ export default function Chat() {
     }, [selectedUserId])
 
     function connectToWs() {
-        const ws = new WebSocket('ws://localhost:4040');
+        // const ws = new WebSocket('ws://localhost:4040');
+        const ws = new WebSocket('ws://mern-chat-app-api-g3qd.onrender.com');
         setWs(ws);
         ws.addEventListener('message', handleMessage);
         ws.addEventListener('close', () => {
